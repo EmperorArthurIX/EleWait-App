a9240b1680748c4344d70895d48680ac3e483459
Ishaan Sunita Pandita <emperorarthurix@gmail.com>
2022-12-28 21:46:48 +0530

    Improving Lift Floor Number Generation

--- app/src/main/java/com/example/loginregisterfirebase1/MainActivity.java
+++ app/src/main/java/com/example/loginregisterfirebase1/MainActivity.java
@@ -3,40 +3,46 @@ package com.example.loginregisterfirebase1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    static int MINFLOOR = 0, MAXFLOOR = 9, DELAY=5000;
+    static int MINFLOOR = 0, MAXFLOOR = 9, DELAY=10*1000;
+    static int nextFloor = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         TextView floorNumberText = findViewById(R.id.floornumber);
+        Button refreshBtn = findViewById(R.id.refreshDataBtn);
 
+        refreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                runLift(floorNumberText);
+            }
+        });
     }
-    protected static void runLift(TextView floorNumber)
+
+    protected static void runLift(TextView floorNumberTxt)
+    {
+        int floor = Integer.parseInt(floorNumberTxt.getText().toString());
+        if(nextFloor == floor) nextFloor = getNextFloor(floor);
+        if (nextFloor > floor)
+            moveLiftUp(floorNumberTxt);
+        else moveLiftDown(floorNumberTxt);
+    }
+
+    protected static void moveLiftUp(TextView floorNumber)
     {
         int floor = Integer.parseInt(floorNumber.getText().toString());
-        int nextFloor = getNextFloor(floor);
-        int floorDiff = Math.abs(floor - nextFloor);
-        for (int i = 1; i <= floorDiff; i++)
-        {
-            try {
-                Thread.sleep(DELAY);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            if(floor < nextFloor)
-                floorNumber.setText(floor + i);
-            else
-                floorNumber.setText(floor - i);
-        }
-        try {
-            Thread.sleep((int)(Math.random() * 2 * DELAY));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        floorNumber.setText(""+(floor+1));
+    }
+    protected static void moveLiftDown(TextView floorNumber)
+    {
+        int floor = Integer.parseInt(floorNumber.getText().toString());
+        floorNumber.setText(""+(floor-1));
     }
     protected static int getNextFloor(int currentFloor)
     {
@@ -47,6 +53,6 @@ public class MainActivity extends AppCompatActivity {
         int val = (int) (Math.random() * (MAXFLOOR+1));
         while(val == currentFloor)
             val = (int) (Math.random() * (MAXFLOOR+1));
-        return val;
+        return Math.random() > 0.5 ? Math.random() > 0.5? MAXFLOOR : MINFLOOR : val;
     }
 }
