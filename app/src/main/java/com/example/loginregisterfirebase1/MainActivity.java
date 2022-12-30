e3335938690ec816a79fdd30c9afb85a3a7ee71d
Ishaan Sunita Pandita <emperorarthurix@gmail.com>
2022-12-30 22:19:34 +0530

    Removed Charts, added numerical stats

--- app/src/main/java/com/example/loginregisterfirebase1/MainActivity.java
+++ app/src/main/java/com/example/loginregisterfirebase1/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.loginregisterfirebase1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     static int MINFLOOR = 0, MAXFLOOR = 9, DELAY=10*1000;
     static int nextFloor = 0;
+    DatabaseReference dbref= FirebaseDatabase.getInstance().getReferenceFromUrl("https://loginregister-b7bea-default-rtdb.firebaseio.com/");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +34,33 @@ public class MainActivity extends AppCompatActivity {
         refreshBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Date now = new Date();
+                long timestamp = now.getTime() / 1000L;
+//                System.out.println(timestamp);
                 runLift(floorNumberText);
+                dbref.child("liftStatus").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChild(Long.toString(timestamp)))
+                            Toast.makeText(MainActivity.this, "Too soon to move lift", Toast.LENGTH_SHORT).show();
+                        else{
+
+                            try {
+                                dbref.child("liftStatus").child(timestamp+"").child("floor").setValue(floorNumberText.getText().toString());
+
+                                Toast.makeText(MainActivity.this, "Floor registered successfully", Toast.LENGTH_SHORT).show();
+                            }
+                            catch (Exception E){
+                                Log.v("Error",E.toString());
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
         });
 
--- app/src/main/java/com/example/loginregisterfirebase1/Stats.java
+++ app/src/main/java/com/example/loginregisterfirebase1/Stats.java
@@ -1,28 +1,51 @@
 package com.example.loginregisterfirebase1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.anychart.anychart.AnyChart;
-import com.anychart.anychart.AnyChartView;
-import com.anychart.anychart.Cartesian;
-import com.anychart.anychart.Pie;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+import java.util.Iterator;
 
 public class Stats extends AppCompatActivity {
     DatabaseReference dbref = FirebaseDatabase.getInstance().getReferenceFromUrl("https://loginregister-b7bea-default-rtdb.firebaseio.com/");
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        dbref.orderByChild("liftStatus").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.hasChild("liftStatus"))
+                {
+                    long totalTrips = snapshot.child("liftStatus").getChildrenCount();
+                    TextView totalFloors = findViewById(R.id.totalFloors);
+                    totalFloors.setText(totalTrips+"");
+                    Iterable<DataSnapshot> timewisefloors = snapshot.child("liftStatus").getChildren();
+                    Iterator<DataSnapshot> itr = timewisefloors.iterator();
+                    TextView recentFloor = findViewById(R.id.recentFloor);
+                    while(itr.hasNext())
+                        recentFloor.setText(itr.next().getValue().toString().substring(7,8));
+                }
+                else
+                    Toast.makeText(Stats.this, "No Lift Data", Toast.LENGTH_SHORT).show();
+            }
 
-        AnyChartView floorFreqChart = findViewById(R.id.floorFrequency);
-        AnyChartView floorTrendChart = findViewById(R.id.floorTrend);
 
-        Cartesian freqs = AnyChart.column();
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
+            }
+        });
     }
 }
